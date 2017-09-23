7a91fe7eb7f9387794a932025da154b61ae8603b
Harm Brouwer <me@hbrouwer.eu>
2017-09-23 12:31:23 +0200

    work on dfs_sampling.pl

--- src/dfs_sampling.pl
+++ src/dfs_sampling.pl
@@ -45,63 +45,79 @@ constants_and_universe(Cs,Um) :-
         findall(C,user:constant(C),Cs),
         length(Cs,N),
         dfs_entities(N,Um).
-
+        
 dfs_sample_properties(Ps,Um,G,CIs,Vm0,Vm1) :-
         findall(C,user:constraint(C),Cs),
-        dfs_interpretation:conjoin(Cs,LCs),
-        negate_and_simplify(Cs,Cs1),
-        dfs_interpretation:conjoin(Cs1,DCs),
-        %write(LCs), nl,
-        %write(DCs), nl,
-        dfs_sample_properties_(Ps,Um,G,CIs,LCs,Vm0,DCs,Vm0,Vm1).
-        
-negate_and_simplify([],[]) :- !.
-negate_and_simplify([C0|Cs0],[C1|Cs1]) :-
-        simplify_formula(neg(C0),C1),
-        negate_and_simplify(Cs0,Cs1).
+        random_permutation(Ps,Ps1),
+        dfs_sample_properties_(Ps1,Um,G,CIs,Cs,Vm0,Vm0,Vm1).
 
-simplify_formula(neg(neg(P0)),P1) :-
-        !, simplify_formula(P0,P1).
-simplify_formula(neg(forall(X,P0)),exists(X,P1)) :-
-        !, simplify_formula(neg(P0),P1).
-simplify_formula(neg(imp(P0,Q0)),and(P1,Q1)) :-
-        !, simplify_formula(P0,P1),
-        simplify_formula(neg(Q0),Q1).
-% simplify_formula(forall(X,imp(P0,Q0)),imp(exists(X,P1),forall(X,imp(P1,Q1)))) :-
-%         !, simplify_formula(P0,P1),
-%         simplify_formula(Q0,Q1).
-% simplify_formula(forall(X,forall(Y,imp(P0,Q0))),imp(exists(X,exists(Y,P1)),forall(X,forall(Y,imp(P1,Q1))))) :-
-%         !, simplify_formula(P0,P1),
-%         simplify_formula(Q0,Q1).
-simplify_formula(P,P).
-
-dfs_sample_properties_([],_,_,_,_,Vm,_,_,Vm) :- !.
-dfs_sample_properties_([P|Ps],Um,G,CIs,LCs,LVm0,DCs,DVm0,LVm3) :-
+dfs_sample_properties_([],_,_,_,_,Vm,_,Vm) :- !.
+dfs_sample_properties_([P|Ps],Um,G,CIs,Cs,LVm0,DVm0,LVm3) :-
+        write(P), write(' '),
         P =.. [Prop|Args],
         dfs_terms_to_entities(Args,CIs,Es),
         add_property(LVm0,Prop,Es,LVm1),
         add_property(DVm0,Prop,Es,DVm1),
-        ( satisfies_constraints(LCs,(Um,LVm1),DCs,(Um,DVm0),G) -> S1 = 1 ; S1 = 0 ),     %% light world
-        ( satisfies_constraints(LCs,(Um,LVm0),DCs,(Um,DVm1),G) -> S0 = 1 ; S0 = 0 ),     %% dark world
+        ( satisfies_constraints(Cs,(Um,LVm1),(Um,DVm0),G) -> S1 = 1 ; S1 = 0 ),     %% light world
+        ( satisfies_constraints(Cs,(Um,LVm0),(Um,DVm1),G) -> S0 = 1 ; S0 = 0 ),     %% dark world
+        write(S1), write(' '), write(S0), write(' '),
         (  S0 == 1, S1 == 1             %% undecided
-        -> probability(P,LVm0,Pr),
+        -> probability(P,LVm0,Pr), !,
            (  maybe(Pr)
-           -> dfs_sample_properties_(Ps,Um,G,CIs,LCs,LVm1,DCs,DVm0,LVm3)
-           ;  dfs_sample_properties_(Ps,Um,G,CIs,LCs,LVm0,DCs,DVm1,LVm3)
+           -> write('\t-> Flip to Light!\n'),
+              dfs_sample_properties_(Ps,Um,G,CIs,Cs,LVm1,DVm0,LVm3)
+           ;  write('\t-> Flip to Dark!\n'),
+              dfs_sample_properties_(Ps,Um,G,CIs,Cs,LVm0,DVm1,LVm3)
            )
         ;  (  S0 == 0, S1 == 1          %% light world
-           -> dfs_sample_properties_(Ps,Um,G,CIs,LCs,LVm1,DCs,DVm0,LVm3)
+           -> write('\t-> Light!\n'),
+             dfs_sample_properties_(Ps,Um,G,CIs,Cs,LVm1,DVm0,LVm3)
            ;  (  S0 == 1, S1 == 0       %% dark world
-              -> dfs_sample_properties_(Ps,Um,G,CIs,LCs,LVm0,DCs,DVm1,LVm3)
-              ;  false                  %% XXX: ?? inconsistent ??
+              -> write('\t-> Dark!\n'),
+                 dfs_sample_properties_(Ps,Um,G,CIs,Cs,LVm0,DVm1,LVm3)
+              ;  write('--inconsistent--\n'),
+                 false                  %% XXX: ?? inconsistent ??
               )                
            )
         ).
 
-satisfies_constraints(LCs,LM,_,_,G) :-  %% light world
-        dfs_interpret(LCs,LM,G), !.%, write('Light!\n').
-satisfies_constraints(_,_,DCs,DM,G) :-  %% dark world
-        dfs_interpret(DCs,DM,G), !.%, write('Dark!\n').
+satisfies_constraints([],_,_,_) :- !, true.
+satisfies_constraints([C|Cs],LM,DM,G) :-
+        dfs_interpret(C,LM,G), !,
+        satisfies_constraints(Cs,LM,DM,G).
+satisfies_constraints([C|Cs],LM,DM,G) :-
+        complement(C,Cc),
+        \+ dfs_interpret(Cc,DM,G),
+        satisfies_constraints(Cs,LM,DM,G).
+
+complement(and(P0,Q0),or(P1,Q1)) :- 
+        !,
+        complement(P0,P1),
+        complement(Q0,Q1).
+complement(or(P0,Q0),and(P1,Q1)) :-
+        !,
+        complement(P0,P1),
+        complement(Q0,Q1).
+complement(imp(P0,Q0),and(neg(P1),Q1)) :-
+        !,
+        complement(P0,P1),
+        complement(Q0,Q1).
+% complement(exists(X,and(P0,Q0)),forall(X,imp(P1,Q1))) :-
+%         !,
+%         complement(P0,P1),
+%         complement(Q0,Q1).
+complement(exists(X,P0),forall(X,P1)) :-
+        !,
+        complement(P0,P1).
+% complement(forall(X,imp(P0,Q0)),exists(X,and(P1,Q1))) :-
+%         !,
+%         complement(P0,P1),
+%         complement(Q0,Q1).
+complement(forall(X,P0),exists(X,P1)) :-
+        !,
+        complement(P0,P1).
+complement(neg(P),neg(P)) :- !.
+complement(P,P).
 
 add_property([],Prop,[E|Es],[P1]) :-
         !,
@@ -111,8 +127,8 @@ add_property([],Prop,[E|Es],[P1]) :-
 add_property([P0|P0s],Prop,[E|Es],[P1|P0s]) :-
         P0 =.. [Prop|_], !,
         (  Es == []
-        -> P0 =.. [Prop|Es0],           %% unary predicates
-           P1 =.. [Prop|[[E]|Es0]]      
+        -> P0 =.. [Prop|[Es0]],         %% unary predicates
+           P1 =.. [Prop|[[E|Es0]]]      
         ;  P0 =.. [Prop|[Es0]],         %% n-ary predicates
            P1 =.. [Prop|[[[E|Es]|Es0]]] ).
 add_property([P0|P0s],Prop,Es,[P0|P1s]) :-
