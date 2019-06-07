4b1d6b41d6779e943863d097031da4eddd53d6d2
Harm Brouwer <me@hbrouwer.eu>
2019-06-07 13:36:21 +0200

    doc/ -> docs/

--- docs.pl
+++ docs.pl
@@ -1,5 +1,5 @@
 :- use_module(library(doc_files)).
 :- consult('src/dfs_main.pl').
 :- write('Generating documentation ...\n').
-:- doc_save(src, [recursive(true), doc_root(doc), title='DFS Tools']).
+:- doc_save(src, [recursive(true), doc_root(docs), title='DFS Tools']).
 :- halt.
